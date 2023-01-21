1d3c848ed657726d41261cd39c7cc5cc3df1dd6e
Pepijn de Vos <pepijndevos@gmail.com>
2023-01-21 19:59:15 +0100

    update arduino code for v2 PCB

--- micromouse/micromouse.ino
+++ micromouse/micromouse.ino
@@ -20,21 +20,31 @@ void setup() {
   Joystick.setRyAxisRange(-512, 512);
   Joystick.setRzAxisRange(-512, 512);
 
-  i1 = analogRead(A3);
-  i2 = analogRead(A2);
-  i3 = analogRead(A1);
-  i4 = analogRead(A9);
-  i5 = analogRead(A8);
-  i6 = analogRead(A7);
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+
+  i1 = analogRead(A0);
+  i2 = analogRead(A9);
+  i3 = analogRead(A10);
+  i4 = analogRead(A2);
+  i5 = analogRead(A3);
+  i6 = analogRead(A1);
 }
 
 void loop() {
-  int s1 = analogRead(A3)-i1;
-  int s2 = analogRead(A2)-i2;
-  int s3 = analogRead(A1)-i3;
-  int s4 = analogRead(A9)-i4;
-  int s5 = analogRead(A8)-i5;
-  int s6 = analogRead(A7)-i6;
+  int s1 = analogRead(A0)-i1;
+  int s2 = analogRead(A9)-i2;
+  int s3 = analogRead(A10)-i3;
+  int s4 = analogRead(A2)-i4;
+  int s5 = analogRead(A3)-i5;
+  int s6 = analogRead(A1)-i6;
 
   int cm1 = s1-s2;
   int dm1 = s1+s2;
@@ -49,13 +59,23 @@ void loop() {
   int ry = dm1+dm2+dm3;
   int rx = cm1/2-cm2+cm3/2;
   int rz = cm1-cm3;
+
+  Joystick.setButton(0, !digitalRead(0));
+  Joystick.setButton(1, !digitalRead(1));
+  Joystick.setButton(2, !digitalRead(2));
+  Joystick.setButton(3, !digitalRead(3));
+  Joystick.setButton(4, !digitalRead(4));
+  Joystick.setButton(5, !digitalRead(5));
+  Joystick.setButton(6, !digitalRead(6));
+  Joystick.setButton(7, !digitalRead(7));
+  Joystick.setButton(8, !digitalRead(8));
   
   Joystick.setYAxis(y);
-  Joystick.setZAxis(-z);
-  Joystick.setXAxis(-x);
+  Joystick.setZAxis(z);
+  Joystick.setXAxis(x);
   Joystick.setRyAxis(-ry);
-  Joystick.setRxAxis(rx);
-  Joystick.setRzAxis(-rz);
+  Joystick.setRxAxis(-rx);
+  Joystick.setRzAxis(rz);
   
   Joystick.sendState();
 }
